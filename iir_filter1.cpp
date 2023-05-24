08fca801165c91473ad93c907dffc07ae3709930
yangrui9501 <yang77567789@gmail.com>
2023-05-24 19:18:29 +0800

    1

--- iir_filter1.cpp
+++ iir_filter1.cpp
@@ -1,15 +1,15 @@
 #include "iir_filter1.h"
 
-void IIRFilter1::init(FilterType Type, double Cutoff_Freq, double Sample_Time)
+void IIRFilter1::init(FilterType filter_type, double _fc, double _T)
 {
     u_out = 0.0;
     u_in = 0.0;
     u_in_pre = 0.0;
 
-    fc = Cutoff_Freq;
-    T = Sample_Time;
+    fc = _fc;
+    T = _T;
 
-    switch (Type)
+    switch (filter_type)
     {
     case TYPE_LPF:
         A1C = 1.0 / (2.0 * PI * fc);
@@ -36,12 +36,12 @@ void IIRFilter1::init(FilterType Type, double Cutoff_Freq, double Sample_Time)
     B2D = B2C * T - 2.0 * B1C;
 }
 
-void IIRFilter1::update(double *Input_Data, double *Filtered_Data)
+const double& IIRFilter1::update(const double& input_data)
 {
-    u_in = *Input_Data;
+    u_in = input_data;
 
     u_out = 1.0 / A1D * (-A2D * u_out + B1D * u_in + B2D * u_in_pre);
     u_in_pre = u_in;
 
-    *Filtered_Data = u_out;
+    return u_out;
 }
--- iir_filter1.h
+++ iir_filter1.h
@@ -1,12 +1,12 @@
 /**
  * @file iir_filter1.h
  * @author your name (you@domain.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-06-05, 2023-05-24
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 #pragma once
 
@@ -23,7 +23,6 @@
 class IIRFilter1
 {
 public:
-    // Initialize type of filter, cut-off frequency, and sample time
     enum FilterType
     {
         TYPE_LPF,
@@ -31,8 +30,8 @@ public:
         TYPE_APD
     };
 
-    void init(FilterType Type, double Cutoff_Freq, double Sample_Time);
-    void update(double *Input_Data, double *Filtered_Data);
+    void init(FilterType filter_type, double _fc, double _T);
+    const double& update(const double& input_data);
 
 private:
     double u_in, u_in_pre, u_out;
